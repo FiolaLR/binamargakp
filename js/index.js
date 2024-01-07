cbc1b297d8773c63eb953ca3e1d3b5bc35b59264
FiolaLR <fiolalaurentrafelina@gmail.com>
2024-01-07 19:45:01 +0700

    membuat landing page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById('logo').addEventListener('click', redirectToHome);
+
+
+function redirectToHome() {
+    console.log('Logo clicked! Redirecting to home...');
+   window.location.href = "/pages/index.html"
+  }
+  
